5472d588985779ed261dcec3998ee582708fac52
chaithraamin1 <chaithra.amin98@gmail.com>
2020-03-01 13:33:16 +0530

    bug fixes

--- src/app/add-page/add-page.component.ts
+++ src/app/add-page/add-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router'
-import { ServiceService } from 'src/app/service.service';
+
 @Component({
   selector: 'app-add-page',
   templateUrl: './add-page.component.html',
@@ -10,28 +10,30 @@ import { ServiceService } from 'src/app/service.service';
 })
 export class AddPageComponent implements OnInit {
   empname: any;
-  showdata: boolean = false;
-  adddetails: boolean = false;
-  empform = this.fb.group({
-    empname: ['', Validators.required],
-    id: ['', Validators.required],
-    dept: ['', Validators.required],
-    date: ['', Validators.required],
-    doj: ['', Validators.required],
-    email: ['', Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]
-  })
+  registerForm: FormGroup;
 
+  showdata: boolean = false;
   submitted: boolean = false;
   res: any = [];
   deptname: string;
   list: any = [];
 
-  get f() { return this.empform.controls; }
-  constructor(private fb: FormBuilder, private router: Router, private service: ServiceService) {
-    var modal = document.getElementById('id03');
+  adddetails: boolean = false;
+  empform = this.fb.group({
+    empname: ['', Validators.required],
+    id: ['', Validators.required],
+    dept: ['', Validators.required],
+    
+    doj: ['', Validators.required],
+    email: ['', Validators.required]
+  })
 
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
+
+
+  constructor(private fb: FormBuilder, private router: Router, private formBuilder: FormBuilder) {
+    
+    var modal = document.getElementById('id03');
+     window.onclick = function (event) {
       if (event.target == modal) {
         modal.style.display = "none";
       }
@@ -42,35 +44,64 @@ export class AddPageComponent implements OnInit {
   ngOnInit() {
 
   }
-  get getterinvoicenumber() {
-    return this.empform.value.empname;
-  }
-  get getterid() {
-    return this.empform.value.id;
-  }
-  get getteremail() {
-    return this.empform.value.email;
-  }
-
   submit() {
+  
+  
+    if (this.empform.controls["empname"].valid == false) {
+      document.getElementById("empnameError").style.display = 'block';
+    }
+    else{
+      document.getElementById("empnameError").style.display = 'none';
+    }
+    if (this.empform.controls["dept"].valid == false) {
+      document.getElementById("departmentError").style.display = 'block';
+    }
+    else{
+      document.getElementById("departmentError").style.display = 'none';
+    }
+    if (this.empform.controls["id"].valid == false) {
+      document.getElementById("idError").style.display = 'block';
+    }
+    else{
+      document.getElementById("idError").style.display = 'none';
+    }
+    if (this.empform.controls["doj"].valid == false) {
+      document.getElementById("dateError").style.display = 'block';
+    }
+    else{
+      document.getElementById("dateError").style.display = 'none';
+    }
+    if (this.empform.controls["email"].valid == false) {
+      document.getElementById("emailError").style.display = 'block';
+    }
+    else{
+      document.getElementById("emailError").style.display = 'none';
+    }
+    if(this.empform.invalid)
+    {
+      return false;
+    }
+    this.submitted=true;
    
-     this.res = this.empform.value;
-     this.list.push(this.res)
+
+    this.res = this.empform.value;
+    this.list.push(this.res)
     console.log("list", this.list)
+    
     document.getElementById('id01').style.display = 'none';
   }
- 
+  
   clear() {
     this.empform.controls["empname"].setValue("");
     this.empform.controls["id"].setValue("");
     this.empform.controls["dept"].setValue("");
     this.empform.controls["email"].setValue("");
-    this.empform.controls["date"].setValue("");
+    this.empform.controls["doj"].setValue("");
 
 
   }
   delete(id) {
-     this.list.splice(id, 1)
+    this.list.splice(id, 1)
   }
   add() {
     this.clear();
@@ -78,4 +109,57 @@ export class AddPageComponent implements OnInit {
     document.getElementById('id01').style.display = 'block';
 
   }
+  checkEmpError()
+  {
+    if (this.empform.controls["empname"].valid == false) {
+      document.getElementById("empnameError").style.display = 'block';
+    }
+    else
+    {
+       document.getElementById("empnameError").style.display = 'none';
+    }
+  }
+  checkIdError()
+  {
+    if (this.empform.controls["id"].valid == false) {
+      document.getElementById("idError").style.display = 'block';
+    }
+    else
+    {
+       document.getElementById("idError").style.display = 'none';
+    }
+  }
+  checkDepartment()
+  {
+    if (this.empform.controls["dept"].valid == false) {
+      document.getElementById("departmentError").style.display = 'block';
+    }
+    else
+    {
+       document.getElementById("departmentError").style.display = 'none';
+    }
+  }
+  checkEmail()
+  {
+    if (this.empform.controls["email"].valid == false) {
+      document.getElementById("emailError").style.display = 'block';
+    }
+    else
+    {
+       document.getElementById("emailError").style.display = 'none';
+    }
+
+  }
+  checkDateError()
+  {
+    if (this.empform.controls["doj"].valid == false) {
+      document.getElementById("dateError").style.display = 'block';
+    }
+    else
+    {
+       document.getElementById("dateError").style.display = 'none';
+    }
+  }
+
+
 }
